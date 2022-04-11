aa14471201f5e267c00bb3a15b8d241ac7912972
Carson-Ray <roedlcar@gmail.com>
2022-04-11 13:49:28 -0500

    added model clas

---
+++ Dictionary/src/main/java/com/mobadictionary/DefinitionEntry.java
@@ -0,0 +1,50 @@
+package com.mobadictionary;
+
+import org.json.simple.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DefinitionEntry {
+
+    private String keyword;
+    private String definition;
+    private String game;
+
+    public DefinitionEntry(JSONObject json) {
+        this.keyword = (String) json.get("keyword");
+        this.definition = (String) json.get("definition");
+        this.game = (String) json.get("game");
+    }
+
+    @Override
+    public String toString() {
+        return "{\"keyword\":\"" + this.keyword
+                + "\",\"definition\":\"" + this.definition
+                + "\",\"game\":\"" + this.game + "\"}";
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
