e3d301ae8d907ec6b1db91ff08ddff21379b69ea
qtgiebel <qtgiebel@gmail.com>
2022-04-12 13:19:57 -0500

    update name to be more descriptive

--- Dictionary/src/main/java/com/mobadictionary/GetDefinitions.java
+++ Dictionary/src/main/java/com/mobadictionary/GetDefinitions.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.Context;
@@ -12,7 +11,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.Map;
 
-public class GetAllDefinitions implements RequestHandler<Request,Response> {
+public class GetDefinitions implements RequestHandler<Request,Response> {
 
     AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
     DynamoDBMapper mapper = new DynamoDBMapper(client);
