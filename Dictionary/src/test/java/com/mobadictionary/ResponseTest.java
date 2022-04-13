9cec9fd4e292e2499c3995f6be0065a22331a9f7
Carson-Ray <roedlcar@gmail.com>
2022-04-12 19:34:44 -0500

    adding test for response test

---
+++ Dictionary/src/test/java/com/mobadictionary/ResponseTest.java
@@ -0,0 +1,18 @@
+package com.mobadictionary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ResponseTest {
+
+    @Test
+    void insertDefinitionEntrySuccess() {
+        DefinitionEntry entry = new DefinitionEntry();
+        Response response = new Response();
+        response.insert(entry);
+
+        assertEquals(1, response.getDefinitions().size());
+    }
+
+}
